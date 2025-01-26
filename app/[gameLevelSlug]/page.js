79275e21e18d037a7a965641dda3a207606c310f
Govind Dandekar <govind.dandekar@gmail.com>
2025-01-25 16:05:04 -0800

    update server action implementation notes

--- app/[gameLevelSlug]/page.js
+++ app/[gameLevelSlug]/page.js
@@ -1,9 +1,12 @@
-import { getQuestions } from "../../lib/questions";
+import { getQuestions } from "../../lib/actions";
 
+// need to setup SQLite DB to store data
 async function GamePage({ params }) {
   const { gameLevelSlug } = await params;
 
-  const questions = getQuestions(gameLevelSlug);
+  // must put getQuestions in <form action={get Questions} />
+  // bind selected level to Server Action with updateQuestions.bind()
+  const questions = await getQuestions(gameLevelSlug);
   console.log(questions);
 
   return <h1>Game Level Page</h1>;
