006d71cef00c127f8c36cd746c15a35459b4a5df
Govind Dandekar <govind.dandekar@gmail.com>
2025-02-10 17:04:11 -0800

    add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import { Chewy } from "next/font/google";
 
 import "./globals.css";
@@ -20,6 +22,7 @@ function RootLayout({ children }) {
         <div className="flex flex-col items-center justify-center min-h-screen bg-cyan-50">
           <div className="flex flex-col bg-cyan-400 rounded-2xl w-3/4 h-[40rem] text-white text-center items-center justify-center">
             {children}
+            <Analytics />
           </div>
           <CustomFooter />
         </div>
