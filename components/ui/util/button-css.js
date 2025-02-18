9069cdb551a9f1ecffebb7d3bce0ebeadb1c1ff4
Govind Dandekar <govind.dandekar@gmail.com>
2025-02-18 14:18:11 -0800

    updates to button css for mobile

--- components/ui/util/button-css.js
+++ components/ui/util/button-css.js
@@ -1,8 +1,8 @@
 export const baseButtonCSS =
-  "bg-cyan-600 py-4 px-6 rounded-2xl mt-12 text-lg md:text-3xl md:py-8 md:px-12 hover:bg-cyan-900 hover:scale-110 transition delay-100 duration-300";
+  "bg-cyan-600 py-2 px-4 rounded-2xl mt-12 text-lg md:text-3xl md:py-8 md:px-12 hover:bg-cyan-900 hover:scale-110 transition delay-100 duration-300";
 
 export const selectedButtonCSS =
-  "bg-cyan-900 py-4 px-6 rounded-2xl mt-12 text-lg md:text-3xl md:py-8 md:px-12 outline-4 outline-offset-2 outline-dotted hover:scale-105 transition delay-100 duration-300";
+  "bg-cyan-900 py-2 px-4 rounded-2xl mt-12 text-lg md:text-3xl md:py-8 md:px-12 outline-4 outline-offset-2 outline-dotted hover:scale-105 transition delay-100 duration-300";
 
 export const answerGridBaseButtonCSS =
   "bg-cyan-600 rounded-2xl text-lg md:text-3xl hover:bg-cyan-900 hover:scale-110 transition delay-100 duration-300";
