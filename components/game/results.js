93829105b08c3f6ea3508a4e1a72d337747d7258
Govind Dandekar <govind.dandekar@gmail.com>
2025-02-20 12:22:48 -0800

    refactor Results to remove duplication of code

--- components/game/results.js
+++ components/game/results.js
@@ -3,69 +3,67 @@ import Link from "next/link";
 
 import SubmitButton from "@/components/ui/submit-button";
 
+const DYNAMIC_RESULTS_ARRAY = [
+  {
+    image: (
+      <Image
+        src="/bingo-trifficult.png"
+        width={128}
+        height={200}
+        alt="bingo playing a xylophone"
+      />
+    ),
+    text: 'Bingo says "Good try! Those questions were really trifficult!"',
+  },
+  {
+    image: (
+      <Image
+        src="/bingo-celebrating.png"
+        width={128}
+        height={200}
+        alt="bingo celebrating"
+      />
+    ),
+    text: 'Bingo says "Hooray! Great work!"',
+  },
+  {
+    image: (
+      <Image
+        src="/bluey-bingo-car.png"
+        width={160}
+        height={120}
+        alt="bluey and bingo playing with a toy car"
+      />
+    ),
+    text: 'Bluey & Bingo say "Wackadoo! You\'re amazing!"',
+  },
+];
+
 function Results({ correctAnswerCounter }) {
+  let resultsIndex;
+
   if (correctAnswerCounter < 4) {
-    return (
-      <>
-        <Image
-          src="/bingo-trifficult.png"
-          width={128}
-          height={200}
-          alt="bingo playing a xylophone"
-        />
-        <p className="text-2xl md:text-5xl mt-8">
-          Correct Answers: {correctAnswerCounter} out of 10!
-        </p>
-        <p className="text-2xl md:text-3xl mt-6">
-          Bingo says &quot;Good try! Those questions were really
-          trifficult!&quot;
-        </p>
-        <SubmitButton>
-          <Link href="/select-level">Play Again!</Link>
-        </SubmitButton>
-      </>
-    );
+    resultsIndex = 0;
   } else if (correctAnswerCounter < 8) {
-    return (
-      <>
-        <Image
-          src="/bingo-celebrating.png"
-          width={128}
-          height={200}
-          alt="bingo celebrating"
-        />
-        <p className="text-2xl md:text-5xl mt-8">
-          Correct Answers: {correctAnswerCounter} out of 10!
-        </p>
-        <p className="text-2xl md:text-4xl mt-6">
-          Bingo says &quot;Hooray! Great work!&quot;
-        </p>
-        <SubmitButton>
-          <Link href="/select-level">Play Again!</Link>
-        </SubmitButton>
-      </>
-    );
+    resultsIndex = 1;
   } else {
-    return (
-      <>
-        <Image
-          src="/bluey-bingo-car.png"
-          width={160}
-          height={120}
-          alt="bluey and bingo playing with a toy car"
-        />
-        <p className="text-2xl md:text-5xl mt-8">
-          Correct Answers: {correctAnswerCounter} out of 10!
-        </p>
-        <p className="text-2xl md:text-4xl mt-6">
-          Bluey & Bingo say &quot;Wackadoo! You&apos;re amazing!&quot;
-        </p>
-        <SubmitButton>
-          <Link href="/select-level">Play Again!</Link>
-        </SubmitButton>
-      </>
-    );
+    resultsIndex = 2;
   }
+
+  return (
+    <>
+      {DYNAMIC_RESULTS_ARRAY[resultsIndex].image}
+      <p className="text-2xl md:text-5xl mt-8">
+        Correct Answers: {correctAnswerCounter} out of 10!
+      </p>
+      <p className="text-2xl md:text-4xl mt-6 px-2">
+        {DYNAMIC_RESULTS_ARRAY[resultsIndex].text}
+      </p>
+      <SubmitButton>
+        <Link href="/select-level">Play Again!</Link>
+      </SubmitButton>
+    </>
+  );
 }
 
 export default Results;
