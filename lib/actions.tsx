adde53a4764edced4b101b5b54bbd61183e18952
Govind Dandekar <govind.dandekar@gmail.com>
2025-04-14 11:05:34 -0700

    delete old discrete LLM calls

--- lib/actions.tsx
+++ lib/actions.tsx
@@ -14,9 +14,6 @@ import { DUMMY_QUESTIONS_EASY, DUMMY_QUESTIONS_MEDIUM, DUMMY_QUESTIONS_HARD } fr
 const aiPromptStart = "please provide an array containing 10 quiz questions related to the kids tv show \"Bluey.\"  each question should have 4 possible answers, one of which is the correct answer.  the correct answer should be labelled as \"true\" and the incorrect answers should be labelled as \"false\".  out of three possible settings of \"easy\", \"medium\" and \"hard\", the user has requested "
 const aiPromptEnd = "questions.  the position of the correct answer should be randomized.\n "
 
-
-
-
 // vercel implementation
 export async function getQuestionsVercel(level: string, selectedModel: string){
   let apiModel: LanguageModelV1 = groq("deepseek-r1-distill-llama-70b");
@@ -49,76 +46,6 @@ export async function getQuestionsVercel(level: string, selectedModel: string){
   }
 }
 
-export async function getQuestionsGeminiVercel(level: string){
-  try {
-    const { object } = await generateObject({
-      model: google("gemini-2.0-flash-001"),
-      schema: z.object({
-        questionsArray: z.array(z.object({question: z.string(), answers: z.array(z.object({text: z.string(), correct: z.boolean()}))}))
-      }),
-      prompt: "please provide an array containing 10 quiz questions related to the kids tv show \"Bluey.\"  each question should have 4 possible answers, one of which is the correct answer.  the correct answer should be labelled as \"true\" and the incorrect answers should be labelled as \"false\".  out of three possible settings of \"easy\", \"medium\" and \"hard\", the user has requested " + level + "questions.  the position of the correct answer should be randomized.\n "
-    })
-
-    return object.questionsArray;
-    /* eslint-disable @typescript-eslint/no-explicit-any */
-  } catch (error: any) {
-    console.error('Error retrieving data from Gemini. Fallback to static dummy questions');
-    console.error("Gemini error: "  + error.message );
-    return getQuestionsDummy(level)
-  }
-}
-
-export async function getQuestionsGroqVercel(level: string){
-  try {
-    const { object } = await generateObject({
-      model: groq("meta-llama/llama-4-scout-17b-16e-instruct"),
-      schema: z.object({
-        questionsArray: z.array(z.object({question: z.string(), answers: z.array(z.object({text: z.string(), correct: z.boolean()}))}))
-      }),
-      prompt: "please provide an array containing 10 quiz questions related to the kids tv show \"Bluey.\"  each question should have 4 possible answers, one of which is the correct answer.  the correct answer should be labelled as \"true\" and the incorrect answers should be labelled as \"false\".  out of three possible settings of \"easy\", \"medium\" and \"hard\", the user has requested " + level + "questions.  the position of the correct answer should be randomized.\n "
-    })
-
-    return object.questionsArray;
-    /* eslint-disable @typescript-eslint/no-explicit-any */
-  } catch (error: any) {
-    console.error('Error retrieving data from Groq. Fallback to static dummy questions');
-    console.error("Groq error: "  + error.message );
-    return getQuestionsDummy(level)
-  }
-}
-
-export async function getQuestionsAnthropic(level: string) {
-  try {
-    const anthropic = new Anthropic({
-      apiKey: process.env["ANTHROPIC_API_KEY"],
-    });
-    
-    const msg = await anthropic.messages.create({
-      model: "claude-3-5-sonnet-20241022",
-      max_tokens: 8192,
-      temperature: 1.0,
-      messages: [
-        {
-          "role": "user",
-          "content": [
-            {
-              "type": "text",
-              "text": "please provide an array containing 10 quiz questions related to the kids tv show \"Bluey.\"  each question should have 4 possible answers, one of which is the correct answer.  the correct answer should be labelled as \"true\" and the incorrect answers should be labelled as \"false\".  out of three possible settings of \"easy\", \"medium\" and \"hard\", the user has requested " + level + "questions.  the position of the correct answer should be randomized.\n "
-            }
-          ]
-        }
-      ]
-    });
-    // log for vercel schema purposes
-    // @ts-expect-error/sdk-error
-    return parseClaudeQuizResponse(msg)
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  } catch (error: any) {
-    console.error('Error retrieving data from Claude. Fallback to static dummy questions');
-    console.error("Claude error: "  + error.message )
-    return getQuestionsDummy(level)
-  }
-}
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export async function getQuestionsDummy(level: string): Promise<any>{
   
