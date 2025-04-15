3fe42d5e694cbf8c8b7f1b9b7581160fce597882
Govind Dandekar <govind.dandekar@gmail.com>
2025-04-15 16:43:03 -0700

    add suspense to ScoresPage

--- app/scores/page.js
+++ app/scores/page.js
@@ -1,7 +1,33 @@
 import Image from "next/image";
 import { Suspense } from "react";
 
-export default function Scores() {
+import { getHighScores } from "@/lib/high-score-actions";
+
+function Fallback() {
+  return (
+    <div className="space-y-4 text-nowrap mt-8 text-xl md:text-3xl">
+      <p>
+        Bluey & Bingo are retreiving the high scores (but are eating ice cream
+        first)!
+      </p>
+    </div>
+  );
+}
+
+async function ScoreList() {
+  const highScores = await getHighScores();
+  console.log(highScores);
+
+  return highScores.map((score) => {
+    return (
+      <p className="text-xl md:text-3xl mt-4" key={score.id}>
+        {score.name} {score.score}{" "}
+      </p>
+    );
+  });
+}
+
+export default async function ScoresPage() {
   return (
     <>
       <Image
@@ -11,9 +37,10 @@ export default function Scores() {
         alt="bluey and bingo"
         priority
       />
-      <div className="space-y-4 text-nowrap mt-8 text-xl md:text-3xl">
-        <p>Bluey & Bingo are retreiving the high scores!</p>
-      </div>
+      <Suspense fallback={<Fallback />}>
+        <p className="text-xl md:text-3xl mt-8">Top Scores Today:</p>
+        <ScoreList />
+      </Suspense>
     </>
   );
 }
