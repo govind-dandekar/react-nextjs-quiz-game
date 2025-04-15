af221ecf13cf3bdfaa3ec7e2868fda617845f128
Govind Dandekar <govind.dandekar@gmail.com>
2025-04-14 17:21:22 -0700

    refactor SelectLevel page for server and client rendering

--- app/select-level/page.js
+++ app/select-level/page.js
@@ -1,67 +1,12 @@
-"use client";
-
-import { useState } from "react";
-
-import Image from "next/image";
-import { redirect } from "next/navigation";
-
-import SubmitButton from "@/components/ui/submit-button";
-import SelectButton from "@/components/ui/select-button";
-
-import {
-  baseButtonCSS,
-  selectedButtonCSS,
-} from "@/components/ui/util/button-css";
+import SelectLevelClient from "@/components/client-server/client-components/select-level-client";
+import SelectLevelServer from "@/components/client-server/server-components/select-level-server";
 
 function SelectLevelPage() {
-  const [selectedLevel, setSelectedLevel] = useState("none");
-
-  function levelSelectHandler(levelName) {
-    setSelectedLevel(levelName);
-  }
-
-  const linkText = "/game/" + selectedLevel.toLowerCase();
-
-  // available game levels
-  const levels = ["Easy", "Medium", "Hard"];
-
   return (
     <>
-      <Image
-        src="/bluey-bingo-sitting.png"
-        width={128}
-        height={200}
-        alt="bluey and bingo sitting"
-        priority
-      />
-      {/* level selection container */}
-      <div className="text-3xl text-center mt-8 md:text-6xl">
-        <p>Select Your Level!</p>
-      </div>
-      <div className="flex space-x-4">
-        {levels.map((level) => {
-          return (
-            <SelectButton
-              key={level}
-              buttonText={level}
-              className={
-                level === selectedLevel ? selectedButtonCSS : baseButtonCSS
-              }
-              onSelect={levelSelectHandler}
-            >
-              {level}
-            </SelectButton>
-          );
-        })}
-      </div>
-      <SubmitButton
-        disabled={selectedLevel === "none"}
-        onClick={() => redirect(linkText)}
-      >
-        {selectedLevel === "none"
-          ? "Pick a Level to Get Started!"
-          : `${selectedLevel} Quiz - Click Here!`}
-      </SubmitButton>
+      <SelectLevelClient>
+        <SelectLevelServer />
+      </SelectLevelClient>
     </>
   );
 }
---
+++ components/client-server/client-components/select-level-client.js
@@ -0,0 +1,56 @@
+"use client";
+
+import { useState } from "react";
+
+import { redirect } from "next/navigation";
+
+import SubmitButton from "@/components/ui/submit-button";
+import SelectButton from "@/components/ui/select-button";
+
+import {
+  baseButtonCSS,
+  selectedButtonCSS,
+} from "@/components/ui/util/button-css";
+
+export default function SelectLevelClient({ children }) {
+  const [selectedLevel, setSelectedLevel] = useState("none");
+
+  function levelSelectHandler(levelName) {
+    setSelectedLevel(levelName);
+  }
+
+  const linkText = "/game/" + selectedLevel.toLowerCase();
+
+  // available game levels
+  const levels = ["Easy", "Medium", "Hard"];
+
+  return (
+    <>
+      {children}
+      <div className="flex space-x-4">
+        {levels.map((level) => {
+          return (
+            <SelectButton
+              key={level}
+              buttonText={level}
+              className={
+                level === selectedLevel ? selectedButtonCSS : baseButtonCSS
+              }
+              onSelect={levelSelectHandler}
+            >
+              {level}
+            </SelectButton>
+          );
+        })}
+      </div>
+      <SubmitButton
+        disabled={selectedLevel === "none"}
+        onClick={() => redirect(linkText)}
+      >
+        {selectedLevel === "none"
+          ? "Pick a Level to Get Started!"
+          : `${selectedLevel} Quiz - Click Here!`}
+      </SubmitButton>
+    </>
+  );
+}
