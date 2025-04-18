c9ca81f073a1815ceaa54dabbb6f3452a0501f45
Govind Dandekar <govind.dandekar@gmail.com>
2025-04-17 19:44:37 -0700

    update ResultsClient to Link and revert NextConfig

--- components/client-server/client-components/results-client.js
+++ components/client-server/client-components/results-client.js
@@ -3,9 +3,8 @@ import SubmitButton from "../../ui/submit-button";
 
 export default function ResultsClient({ handleClick }) {
   return (
-    //js-expect-error
-    <a href="/scores">
+    <Link href="/scores" prefetch={false}>
       <SubmitButton onClick={handleClick}>See High Scores!</SubmitButton>
-    </a>
+    </Link>
   );
 }
