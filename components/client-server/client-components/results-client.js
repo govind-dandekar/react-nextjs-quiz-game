eb8243e19c5f96335ba1cfff8370870f815cd57d
Govind Dandekar <govind.dandekar@gmail.com>
2025-04-17 19:31:35 -0700

    update linter in ResultsClient

--- components/client-server/client-components/results-client.js
+++ components/client-server/client-components/results-client.js
@@ -1,9 +1,9 @@
 "use client";
 import SubmitButton from "../../ui/submit-button";
-import Link from "next/link";
 
 export default function ResultsClient({ handleClick }) {
   return (
+    //@ts-ignore: use a to disable prefetching
     <a href="/scores">
       <SubmitButton onClick={handleClick}>See High Scores!</SubmitButton>
     </a>
