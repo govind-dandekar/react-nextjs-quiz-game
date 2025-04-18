dddc5b7a624effc0a014db32e9b9fcb8ac900511
Govind Dandekar <govind.dandekar@gmail.com>
2025-04-17 19:35:29 -0700

    update linter to @ts-expect-error in ResultsClient

--- components/client-server/client-components/results-client.js
+++ components/client-server/client-components/results-client.js
@@ -3,7 +3,7 @@ import SubmitButton from "../../ui/submit-button";
 
 export default function ResultsClient({ handleClick }) {
   return (
-    //ts-expect-error
+    //js-expect-error
     <a href="/scores">
       <SubmitButton onClick={handleClick}>See High Scores!</SubmitButton>
     </a>
