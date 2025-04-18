52a19277c12c758c6a673c8aafae45a842417669
Govind Dandekar <govind.dandekar@gmail.com>
2025-04-17 20:22:28 -0700

    delete Link import from ResultsClient

--- components/client-server/client-components/results-client.js
+++ components/client-server/client-components/results-client.js
@@ -1,6 +1,5 @@
 "use client";
 import SubmitButton from "../../ui/submit-button";
-import Link from "next/link";
 
 export default function ResultsClient({ handleClick }) {
   return <SubmitButton onClick={handleClick}>See High Scores!</SubmitButton>;
