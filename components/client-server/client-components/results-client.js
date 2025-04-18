0e828a617951378d358da728d449a2f5456930b4
Govind Dandekar <govind.dandekar@gmail.com>
2025-04-17 19:23:52 -0700

    add href to Link in ResultsClient

--- components/client-server/client-components/results-client.js
+++ components/client-server/client-components/results-client.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function ResultsClient({ handleClick }) {
   return (
-    <Link prefetch={false}>
+    <Link href="/scores" prefetch={false}>
       <SubmitButton onClick={handleClick}>See High Scores!</SubmitButton>
     </Link>
   );
