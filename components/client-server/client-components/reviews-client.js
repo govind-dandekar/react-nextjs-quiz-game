8b12461f0300dcc03d6225ee0d166a80409ae595
Govind Dandekar <govind.dandekar@gmail.com>
2025-04-20 08:06:35 -0700

    fix vercel build errors

--- components/client-server/client-components/reviews-client.js
+++ components/client-server/client-components/reviews-client.js
@@ -3,7 +3,7 @@ import SubmitButton from "@/components/ui/submit-button";
 export default function ReviewsClient({ addReview }) {
   return (
     <form className="mt-6" action={addReview}>
-      <SubmitButton>Add Muffin's Review!</SubmitButton>
+      <SubmitButton>Add Muffin`&apos;` Review!</SubmitButton>
     </form>
   );
 }
